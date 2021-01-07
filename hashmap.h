caba0a7f8d9e589d97df39d39ca731d8b9540182
Conrad Khakhria <conradjkhakhria@gmail.com>
2021-01-07 15:23:43 +0000

    Changed formatting

--- hashmap.h
+++ hashmap.h
@@ -6,21 +6,21 @@
 /* structures */
 
 // node struct for the collision resolution linked list
-typedef struct _list_node {
-    void * key;
-    void * val;
-    struct _list_node * next;
-} * _list_node;
+struct hashmap_list_node {
+    void* key;
+    void* val;
+    struct hashmap_list_node* next;
+};
 
 // bucket struct for each key/value pair (+ linked list) bucket
-typedef struct _bucket {
+struct hashmap_bucket {
     uint64_t hash;
-    _list_node first_node;
-} _bucket;
+    struct hashmap_list_node* first_node;
+};
 
 // the actual map struct
 typedef struct _map {
-    _bucket * buckets_list;
+    struct hashmap_bucket* buckets_list;
     size_t key_size;
     size_t data_size;
     int item_count;
@@ -30,11 +30,11 @@ typedef struct _map {
 /* map function primitives */
 
 Map makemap(size_t keysize, size_t valsize);
-int map_assign(Map map, void * key, void * val);
-void * map_get(Map map, void * key);
-void map_remove(Map map, void * key);
+int map_assign(Map map, void* key, void* val);
+void* map_get(Map map, void* key);
+void map_remove(Map map, void* key);
 
 /* Other function primitives */
 
-uint64_t FNV1a_hash(void * input, int input_length);
+uint64_t FNV1a_hash(void* input, int input_length);
 void destroy(Map map);
